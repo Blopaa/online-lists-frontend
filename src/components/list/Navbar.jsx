f7464d33e9bb7a5c0a41639e19fafffb69dd319b
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-17 18:55:30 +0200

    created navbar

---
+++ src/components/list/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Router from 'next/router'
+
+const Navbar = () => {
+  const [name, setName] = useState();
+  useEffect(() => {
+    const guardado = localStorage.getItem("User");
+    const user = JSON.parse(guardado);
+    setName(user.user.username);
+  }, []);
+
+  const handleLogout = () => {
+      document.cookie = "auth-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC"
+      Router.push('/signin')
+  }
+
+  return (
+    <div className="navbar__container">
+      <div>{name}</div>
+      <div className="navbar__logout" onClick={handleLogout}>logout</div>
+    </div>
+  );
+};
+
+export default Navbar;
