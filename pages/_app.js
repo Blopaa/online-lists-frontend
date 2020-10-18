c6f842362ad7bc546803ee0901ffbdd14c63540f
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-18 17:11:39 +0200

    fix bugs and add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import LoadingContext from '../src/components/contexts/LoadingContext'
-import '../styles/styles.scss'
+import { useState } from "react";
+import ListsContext from "../src/components/contexts/ListsContext";
+import LoadingContext from "../src/components/contexts/LoadingContext";
+import "../styles/styles.scss";
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [lists, setLists] = useState({});
   return (
-    <LoadingContext.Provider value={{loading, setLoading}}>
-       <Component {...pageProps} />
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      <ListsContext.Provider value={{ lists, setLists }}>
+        <Component {...pageProps} />
+      </ListsContext.Provider>
     </LoadingContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/components/nothing/nothing.jsx
+++ src/components/nothing/nothing.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import {AiOutlineGithub} from 'react-icons/ai'
 
-const nothing = () => {
-    return (
-        <div>
-            hi
-        </div>
-    )
-}
+const Nothing = () => {
+  return (
+    <div className="nothing__container">
+      <p>
+        click one list to show it <br />
+        you also can visit github to see this project
+      </p>
+      <a target="blank" href="https://github.com/Blopaa/online-lists-frontend">
+      <AiOutlineGithub className="pointer" style={{fontSize:"30px"}}/>
+      </a>
+      <p>created with love {"<3"}</p>
+    </div>
+  );
+};
 
-export default nothing
+export default Nothing;
